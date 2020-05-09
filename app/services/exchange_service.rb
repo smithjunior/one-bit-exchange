67c119dcd42e0a8fce2350f30105c1f344ac582c
Smith Júnior <smith.junior@icloud.com>
2020-05-09 20:54:24 -0300

    Exchange Service created

---
+++ app/services/exchange_service.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rest-client"
+require "json"
+
+class ExchangeService
+  def initialize(source_currency, target_currency, amount)
+    @source_currency = source_currency
+    @target_currency = target_currency
+    @amount = amount.to_f
+  end
+
+  def call
+    value = get_exchange
+    value * @amount
+  rescue RestClient::ExceptionWithResponse => e
+    e.response
+  end
+
+  private
+
+  def get_exchange
+    exchange_api_url = Rails.application.credentials[:currency_api_url]
+    exchange_api_key = Rails.application.credentials[:currency_api_key]
+    url_api = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+    response = RestClient.get url_api
+    JSON.parse(response.body)["currency"][0]["value"].to_f
+  end
+end
