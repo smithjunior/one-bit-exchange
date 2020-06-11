33310d281c531a30c6af7aa6c13f48d9acae20f9
Smith Júnior <smith.junior@icloud.com>
2020-06-10 22:18:04 -0300

    Testing deploy

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -22,7 +22,9 @@ class ExchangeService
   def get_exchange
     exchange_api_url = Rails.application.credentials[:currency_api_url]
     exchange_api_key = Rails.application.credentials[:currency_api_key]
+    
     url_api = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+    
     response = RestClient.get url_api
     JSON.parse(response.body)["currency"][0]["value"].to_f
   end
